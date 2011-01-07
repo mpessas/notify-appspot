3fbbb32e6c727f3d5dbaaa84829da1e997be37ca
Apostolos Bessas <mpessas@gmail.com>
2011-01-07 19:38:27 +0200

    Store subscription time as well.

--- jidperson/models.py
+++ jidperson/models.py
@@ -6,7 +6,7 @@ from google.appengine.ext import db
 class JIDPerson(db.Model):
     """Model for the subscribers."""
     jid = db.IMProperty(required=True, indexed=True)
-    reg_date = db.DateProperty(auto_now_add=True)
+    reg_date = db.DateTimeProperty(auto_now_add=True)
         
 
 def create_im(jid):
